ad9d4c7a96832c094fe601bd733a4a2cd878acd0
premtesters <premtesters@gmail.com>
2026-01-22 17:12:53 +0530

    Second Commit

--- test/homepage.spec.js
+++ test/homepage.spec.js
@@ -2,5 +2,5 @@ import { test, expect } from "@chromatic-com/playwright";
 
 test("Homepage", async ({ page }) => {
   await page.goto("https://mealexpress.com/");
-  await expect(page).toHaveTitle(/Meal/i);
+  await expect(page).toHaveTitle(/Mal/i);
 });
